8857dee58e08e0412d80ff29f579a00f9960b98d
hossein <hkhani1384@gmail.com>
2024-07-07 11:20:26 +0330

    add Interval multiplication table

---
+++ Interval_multiplication_table.py
@@ -0,0 +1,7 @@
+a = int(input("aval baze1: "))
+b = int(input("akhar baze1: "))
+c = int(input("aval baze2: "))
+d = int(input("akhar baze2: "))
+for x in range(a,b) :
+    for y in range(c,d) :
+        print((x*y))
