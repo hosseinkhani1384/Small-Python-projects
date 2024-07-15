408f1f853cdec7e4d9228b1f38a90a296dbe87a1
hossein <hkhani1384@gmail.com>
2024-07-15 23:40:34 +0330

    add numbernumber.py

---
+++ numbernumber.py
@@ -0,0 +1,24 @@
+x = int(input("number: ")) 
+
+if x>0 : 
+    if x%2==0 :
+        print(2)
+    if x%3==0 :
+        print(3)
+    if x%4==0 :
+        print(4)
+    if x%5==0 :
+        print(5)
+    if x%6==0 :
+        print(6)
+    if x%7==0 :
+        print(7)
+    if x%8==0 :
+        print(8)
+    if x%9==0 :
+        print(9)    
+    if x%10==0 :
+        print(10)
+
+else :
+    print("adad balatar az seft entekhab konid")
