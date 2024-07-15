b1706dd449ce70f2afd17dd129c579a85a9fcccc
hossein <hkhani1384@gmail.com>
2024-07-15 23:39:56 +0330

    add 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+import math
+n = int(input("n: "))
+l = list()
+for y in range(1,n+1):
+    x = int(input("number"+str(y)+": "))
+    l.append(x)
+def hossein(g):
+    w = ""
+    for p in l:
+        w += str(p)+","
+    return w    
+  
+   
+    
+
+
+        
+            
+    
+
+
+
+        
+        
